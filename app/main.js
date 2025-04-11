de0440088740364f4402c9b0eec4d9aff5e2ee75
Yuvraj Raghuvanshi <124128352+yuvrajraghuvanshi@users.noreply.github.com>
2025-04-11 18:10:09 +0530

    done

--- app/main.js
+++ app/main.js
@@ -12,7 +12,7 @@ const server = net.createServer((socket) => {
     console.log(data);
     const path = data.toString().split(" ")[1];
     const responseStatus = path === "/" ? "200 ok" : "404 Not Found";
-    socket.write(`HTTP/1.1 ${responseStatus} \r\n\r\n`);
+    socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
   });
 });
 
