4a9a17a90d0c9c6c9acd1e47f8224f2395f202c7
Yuvraj Raghuvanshi <124128352+yuvrajraghuvanshi@users.noreply.github.com>
2025-04-12 17:05:38 +0530

    done

--- app/main.js
+++ app/main.js
@@ -9,8 +9,10 @@ const server = net.createServer((socket) => {
     socket.end();
   });
   socket.on("data", (data) => {
+    
     const value = data.toString().split('\r\n');
     const path = data.toString().split(" ")[1];
+    data=data.toString();
     const lines=value.find(v=>v.toLowerCase().startsWith('user-agent:'));
     const showValue=lines? lines.split(': ')[1]: 'Unknown';
     if (path === "/user-agent") {
@@ -18,6 +20,9 @@ const server = net.createServer((socket) => {
     socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${showValue.length}\r\n\r\n${showValue}`
       );
+    }else if(path==="/echo/"){
+        const content = data.split('/echo/')[1];
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
     }else{
         socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
   }});
