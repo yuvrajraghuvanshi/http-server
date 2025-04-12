752ac10aa008c861abea72e49be0500113d3d9b6
Yuvraj Raghuvanshi <124128352+yuvrajraghuvanshi@users.noreply.github.com>
2025-04-12 17:18:40 +0530

    done

--- app/main.js
+++ app/main.js
@@ -9,23 +9,24 @@ const server = net.createServer((socket) => {
     socket.end();
   });
   socket.on("data", (data) => {
-    
-    const value = data.toString().split('\r\n');
+    const value = data.toString().split("\r\n");
     const path = data.toString().split(" ")[1];
-    data=data.toString();
-    const lines=value.find(v=>v.toLowerCase().startsWith('user-agent:'));
-    const showValue=lines? lines.split(': ')[1]: 'Unknown';
+    data = data.toString();
+    const lines = value.find((v) => v.toLowerCase().startsWith("user-agent:"));
+    const showValue = lines ? lines.split(": ")[1] : "Unknown";
     if (path === "/user-agent") {
-    //   socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
-    socket.write(
+      socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${showValue.length}\r\n\r\n${showValue}`
       );
-    }else if(path==="/echo/"){
-        const content = data.split('/echo/')[1];
-            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
-    }else{
-        socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
-  }});
+    } else if (path.includes("/echo/")) {
+      const content = data.split("/echo/")[1];
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
+      );
+    } else {
+      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n`);
+    }
+  });
 });
 
 server.listen(4221, "localhost");
