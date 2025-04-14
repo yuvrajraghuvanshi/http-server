019926c37a8c41f921effd0e02d2341625f4b212
Yuvraj Raghuvanshi <124128352+yuvrajraghuvanshi@users.noreply.github.com>
2025-04-14 21:45:29 +0530

    done

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const fs=require('fs');
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -18,7 +19,18 @@ const server = net.createServer((socket) => {
     const path = data.toString().split(" ")[1];
     if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (path === "/user-agent") {
+    }else if(path.startsWith('/files')){
+      const args=process.argv[2];
+      const fileName=path.replace("/files/","");
+      const filePath=path.join(args,fileName);
+      fs.readFile(filePath,(err,fileContent)=>{
+        if (err) {
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }else{
+          socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n`)
+        }
+      })
+    }else if (path === "/user-agent") {
       const userAgent = value[1];
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
